ac5e8a9233313f1366269301ea5d9967ca15943a
willydlw <willydlw@comcast.net>
2026-02-05 13:19:24 -0700

    completed ch2, lesson 3 get_file_content

---
+++ test_get_file_content.py
@@ -0,0 +1,6 @@
+from functions.get_file_content import get_file_content 
+
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
