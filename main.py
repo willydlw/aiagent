0432991203a7aa733e4ca43f5dfbe934e328de39
willydlw <willydlw@comcast.net>
2026-02-05 09:44:50 -0700

    added --verbose arg

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     # Define the arguments we want to accept
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     # parse the args the user provided 
     args = parser.parse_args() 
 
@@ -22,11 +23,14 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client, messages)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+
+    generate_content(client, messages, args.verbose)
     
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, args.verbose):
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
@@ -36,8 +40,10 @@ def generate_content(client, messages):
     if response is None or response.usage_metadata is None:
         raise RuntimeError("Gemini API response appears to be malformed")
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose: 
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print("Response:")
     print(response.text)
    
